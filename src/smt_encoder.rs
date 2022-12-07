f9600c6b7a9a59a88102f3f50f9f5d23e9cd2541
Leo Alt <leo@ethereum.org>
2022-12-07 10:45:17 +0100

    encode smt expressions

--- src/smt_encoder.rs
+++ src/smt_encoder.rs
@@ -1,7 +1,7 @@
 use std::fmt;
 
 use crate::{
-    ast::{ConnectionIdentity, Expression, PermutationIdentity, Pil, PlookupIdentity, PublicCell},
+    ast::*,//{ConnectionIdentity, Expression, PermutationIdentity, Pil, PlookupIdentity, PolIdentity, PublicCell},
     smt::*,
     visitor::*,
 };
@@ -97,6 +97,18 @@ impl Visitor for SmtEncoder {
         unimplemented!("declaration of public values is not supported")
     }
 
+    fn visit_polynomial_identity(
+        &mut self,
+        i: &PolIdentity,
+        ctx: &Pil,
+    ) -> Result<Self::Error> {
+        let constr = &ctx.expressions[i.e.0];
+        let expr = eq_zero(self.encode_expression(constr, ctx));
+        let fun = define_fun(SMTVariable::new(format!("constr_{}", 1).to_string(), SMTSort::Bool), vec![], expr);
+        println!("{}", fun.as_smt());
+        unimplemented!("Found polynomial identity {:?} which is not supported", i);
+    }
+
     fn visit_connection_identity(
         &mut self,
         i: &ConnectionIdentity,
@@ -158,6 +170,80 @@ impl Visitor for SmtEncoder {
         }
 
         Ok(())
+    }
+}
+
+impl SmtEncoder {
+    fn encode_expression(&self, e: &Expression, ctx: &Pil) -> SMTExpr {
+        match e {
+            /*
+               Expression::Public(w) => {
+               encode_public(&w.inner)
+               }
+               Expression::Neg(w) => {
+               encode_neg(&w.inner)
+               }
+               Expression::Exp(w) => {
+               encode_exp(&w.inner)
+               }
+               */
+            Expression::Add(w) => {
+                self.encode_add(&w.inner, ctx)
+            },
+            Expression::Sub(w) => {
+                self.encode_sub(&w.inner, ctx)
+            },
+            Expression::Mul(w) => {
+                self.encode_mul(&w.inner, ctx)
+            },
+            Expression::Cm(w) => {
+                self.encode_cm(&w.inner, ctx)
+            },
+            Expression::Number(w) => {
+                self.encode_number(&w.inner)
+            },
+            Expression::Const(w) => {
+                self.encode_const(&w.inner, ctx)
+            },
+            _ => panic!()
+        }
+    }
+
+    fn encode_add(&self, expr: &Add, ctx: &Pil) -> SMTExpr {
+        add(
+            self.encode_expression(&expr.values[0], ctx),
+            self.encode_expression(&expr.values[1], ctx)
+           )
+    }
+
+    fn encode_sub(&self, expr: &Sub, ctx: &Pil) -> SMTExpr {
+        sub(
+            self.encode_expression(&expr.values[0], ctx),
+            self.encode_expression(&expr.values[1], ctx)
+           )
+    }
+
+    fn encode_mul(&self, expr: &Mul, ctx: &Pil) -> SMTExpr {
+        mul(
+            self.encode_expression(&expr.values[0], ctx),
+            self.encode_expression(&expr.values[1], ctx)
+           )
+    }
+
+    fn encode_cm(&self, expr: &Cm, ctx: &Pil) -> SMTExpr {
+        let (key, _) = ctx.get_cm_reference(expr);
+        let key = key.clone().replace('.', "_");
+        SMTVariable::new(key, SMTSort::Int).into()
+    }
+
+    fn encode_number(&self, n: &Number) -> SMTExpr {
+        literal(n.value.clone(), SMTSort::Int)
+    }
+
+    fn encode_const(&self, c: &Const, ctx: &Pil) -> SMTExpr {
+        let (key, _) = ctx.get_const_reference(&c);
+        let key = key.clone().replace('.', "_");
+        SMTVariable::new(key, SMTSort::Int).into()
     }
 }
 
