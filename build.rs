29582dd971b519bcce390508b34cadc1d4f63673
schaeff <thibaut@schaeff.fr>
2022-12-08 10:05:38 +0100

    simplify build.rs

--- build.rs
+++ build.rs
@@ -1,35 +1,15 @@
-use std::path::PathBuf;
-use walkdir::WalkDir;
+use std::process::Command;
 
 fn main() {
-    // compile all pil files
-    use std::process::Command;
+    // update submodules
+    let _ = Command::new("git")
+        .args(["submodule", "update"])
+        .output()
+        .expect("process failed to execute");
 
-    use std::env;
-
-    for entry in WalkDir::new("pil") {
-        let entry = entry.unwrap();
-
-        if entry.file_type().is_dir() {
-            let _ =
-                std::fs::create_dir(PathBuf::from(env::var("OUT_DIR").unwrap()).join(entry.path()));
-        } else if entry.file_type().is_file() {
-            let out_file = PathBuf::from(env::var("OUT_DIR").unwrap())
-                .join(entry.path())
-                .with_extension("pil.json");
-
-            let out = Command::new("node")
-                .args([
-                    "pilcom/src/pil.js",
-                    entry.path().as_os_str().to_str().unwrap(),
-                    "-o",
-                    out_file.as_os_str().to_str().unwrap(),
-                ])
-                .output()
-                .expect("process failed to execute");
-
-            eprintln!("{:?}", out);
-        }
-    }
-    println!("cargo:rerun-if-changed=pil");
+    // install pilcom
+    let _ = Command::new("npm")
+        .args(["install", "--prefix", "./pilcom"])
+        .output()
+        .expect("process failed to execute");
 }
