9933e0e9b63d5f26c030dc3d264851001b6313f1
schaeff <thibaut@schaeff.fr>
2022-12-08 09:56:11 +0100

    compile inside pil_json

--- src/lib.rs
+++ src/lib.rs
@@ -8,13 +8,28 @@ mod visitor;
 /// get the path for a compiled PIL file
 #[cfg(test)]
 pub(crate) fn pil_json(f: &str) -> String {
-    std::fs::read_to_string(
-        std::path::PathBuf::from(env!("OUT_DIR"))
-            .join("pil")
-            .join(f)
-            .with_extension("pil.json"),
-    )
-    .unwrap()
+    use std::{path::PathBuf, process::Command};
+
+    let f = PathBuf::from("pil").join(PathBuf::from(f));
+
+    let dir = tempdir::TempDir::new("pil_output").unwrap();
+    std::fs::create_dir_all(dir.path().join(f.clone().parent().unwrap())).unwrap();
+
+    let out_file = dir.path().join(f.clone()).with_extension("pil.json");
+
+    let out = Command::new("node")
+        .args([
+            "pilcom/src/pil.js",
+            f.as_os_str().to_str().unwrap(),
+            "-o",
+            out_file.as_os_str().to_str().unwrap(),
+        ])
+        .output()
+        .expect("process failed to execute");
+
+    println!("{:?}", out);
+
+    std::fs::read_to_string(out_file).unwrap()
 }
 
 #[cfg(test)]
@@ -24,21 +39,21 @@ mod test {
 
     #[test]
     fn parse_main() {
-        let pil_str = pil_json("zkevm/main");
+        let pil_str = pil_json("zkevm/main.pil");
         let pil: Pil = serde_json::from_str(&pil_str).unwrap();
         pil.validate().unwrap();
     }
 
     #[test]
     fn display_adder() {
-        let pil_str = pil_json("adder");
+        let pil_str = pil_json("adder.pil");
         let pil: Pil = serde_json::from_str(&pil_str).unwrap();
         println!("{}", pil);
     }
 
     #[test]
     fn display_main() {
-        let pil_str = pil_json("zkevm/main");
+        let pil_str = pil_json("zkevm/main.pil");
         let pil: Pil = serde_json::from_str(&pil_str).unwrap();
         println!("{}", pil);
     }
--- src/smt_encoder.rs
+++ src/smt_encoder.rs
@@ -169,7 +169,7 @@ mod test {
 
     #[test]
     fn encode_byte4() {
-        let pil_str = pil_json("zkevm/byte4");
+        let pil_str = pil_json("zkevm/byte4.pil");
         let pil: Pil = serde_json::from_str(&pil_str).unwrap();
 
         let smt_pil = SmtPil::new(pil);
